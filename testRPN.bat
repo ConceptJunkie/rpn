1329bac1c144a5224f3a1c9b503d132005544384
Rick Gutleber <rickg@his.com>
2014-06-10 14:41:31 -0400

    First test script
    This is a brain-dead test.  It exercises all of the operators so you can
    make sure none of them crash.

---
+++ testRPN.bat
@@ -0,0 +1,332 @@
+@echo on
+rpn -394 abs
+rpn 0.8 acos
+rpn 0.6 acosh
+rpn 0.4 acot
+rpn 0.3 acoth
+rpn 0.2 acsc
+rpn 0.67 acsch
+rpn 4 3 add
+rpn 13 altfac
+rpn 0x7777 0xdcba and
+rpn apery
+rpn 12 aperynum
+rpn 0.4 asec
+rpn 0.1 asech
+rpn 0.8 asin
+rpn 0.3 asinh
+rpn 0.2 atan
+rpn 0.45 atanh
+rpn avogadro
+rpn 153 balanced
+rpn 153 balanced_
+rpn 45 bell
+rpn 4 5 bellpoly
+rpn 16 bernoulli
+rpn 9 12 binomial
+rpn 773 carol
+rpn 85 catalan
+rpn catalans
+rpn 17 cdecagonal
+rpn 1000 cdecagonal?
+rpn 9.99999 ceiling
+rpn 100 centeredcube
+rpn champernowne
+rpn 0x101 char
+rpn 102 cheptagonal
+rpn 100000 cheptagonal?
+rpn 103 chexagonal
+rpn 104 cnonagonal
+rpn 5,000,000 cnonagonal?
+rpn 10 coctagonal
+rpn 361 coctagonal?
+rpn -a 80 copeland
+rpn 45 degrees cos
+rpn pi 3 / cosh
+rpn pi 7 / cot
+rpn pi 9 / coth
+rpn 0xffff countbits
+rpn 1024 countdiv
+rpn 77 cousinprime
+rpn 108 cpentagonal
+rpn 9999 cpentagonal?
+rpn 108 5 cpolygonal
+rpn 9999 5 cpolygonal?
+rpn pi 12 / csc
+rpn pi 13 / csch
+rpn 5 csquare
+rpn 49 csquare?
+rpn 100 ctriangular
+rpn 10000 ctriangular?
+rpn 3 cube
+rpn 151 decagonal
+rpn 123454321 decagonal?
+rpn 100 delannoy
+rpn 8 million seconds dhms
+rpn 12 13 divide
+rpn 2 3 ** 3 4 ** * divisors
+rpn 1 radian dms
+rpn 44 dodecahedral
+rpn 10 20 ** double
+rpn 54 doublebal
+rpn 54 doublebal_
+rpn 9 doublefac
+rpn e
+rpn 45 67 egypt
+rpn 1 10 range 5 element
+rpn 150 tons estimate
+rpn euler
+rpn 13 exp
+rpn 12 exp10
+rpn 100 expphi
+rpn 1.1 1.1 10 exprange
+rpn 883847311 factor
+rpn 23 factorial
+rpn 39399 fibonacci
+rpn 0x62ffffff float
+rpn 3.4 floor
+rpn 12 23 fraction
+rpn 1234567890 fromunixtime
+rpn 3 gamma
+rpn 2 8 8 georange
+rpn glaisher
+rpn 34 harmonic
+rpn 203 heptagonal
+rpn 99999 heptagonal?
+rpn 224623 heptanacci
+rpn 2039 hepthex
+rpn 8684 heptpent
+rpn 222 heptsquare
+rpn 399 hepttri
+rpn 340 hexagonal
+rpn 230860 hexagonal?
+rpn 949 hexanacci
+rpn 107 hexpent
+rpn 54658 seconds hms
+rpn 4 3 hyper4_2
+rpn 17 hyperfac
+rpn 3 4 hypot
+rpn 3 i
+rpn 100 icosahedral
+rpn 456 8 integer
+rpn 1000 10000 isdivisible
+rpn 102 isolated
+rpn 2049 isprime
+rpn 1024 issquare
+rpn itoi
+rpn 10 jacobsthal
+rpn khinchin
+rpn 8 kynea
+rpn 5 6 lah
+rpn 5 lambertw
+rpn 7 8 leyland
+rpn 10 lgamma
+rpn 12 li
+rpn 1000 ln
+rpn 1000 log10
+rpn 1000 log2
+rpn 6561 3 logxy
+rpn 3456789012 long
+rpn 1234567890123456789012 longlong
+rpn 99 lucas
+rpn e 20 makecf
+rpn maxchar
+rpn maxlong
+rpn maxlonglong
+rpn maxquadlong
+rpn maxshort
+rpn maxuchar
+rpn maxulong
+rpn maxulonglong
+rpn maxuquadlong
+rpn maxushort
+rpn mertens
+rpn minchar
+rpn minlong
+rpn minlonglong
+rpn minquadlong
+rpn minshort
+rpn minuchar
+rpn minulong
+rpn minulonglong
+rpn minuquadlong
+rpn minushort
+rpn 11001 100 modulo
+rpn 56 motzkin
+rpn 5 7 multiply
+rpn 6 8 narayana
+rpn 4 negative
+rpn 554 nonagonal
+rpn 9 6 ** nonagonal?
+rpn 12 nonahept
+rpn 13 nonahex
+rpn 14 nonaoct
+rpn 15 nonapent
+rpn 16 nonasquare
+rpn 17 nonatri
+rpn -x 0xefefefefefefef not
+rpn now
+rpn 34 inches 3 nspherearea
+rpn 3 cubic_meters 4 nsphereradius
+rpn 3 square_feet 6 nspherevolume
+rpn 67 nthprime?
+rpn 453456 nthquad?
+rpn 102 octagonal
+rpn 8 4 ** 1 + octagonal?
+rpn 23 octahedral
+rpn 8 octhept
+rpn 7 octhex
+rpn 6 octpent
+rpn 11 octsquare
+rpn 10 octtri
+rpn 11234 oeis
+rpn 11234 oeiscomment
+rpn 11234 oeisex
+rpn 11234 oeisname
+rpn omega
+rpn -x 0x5543 0x7789 ord
+rpn 76 padovan
+rpn 0xff889d8f parity
+rpn 12 pascal
+rpn 13 pell
+rpn 16 pentagonal
+rpn 5 5 ** 5 + pentagonal?
+rpn 16 pentanacci
+rpn 12 pentatope
+rpn 8 3 perm
+rpn phi
+rpn pi
+rpn plastic
+rpn 1 13 polyarea
+rpn 4 5 polygamma
+rpn 9 12 polygonal
+rpn 12 12 ** 12 polygonal?
+rpn 9 3 polylog
+rpn 4 3 polyprime
+rpn 8 7 polytope
+rpn 4 5 power
+rpn 8783 prime
+rpn 8783 prime?
+rpn 87 primepi
+rpn 3 8 primes
+rpn 45 primorial
+rpn 304 pyramid
+rpn 17 quadprime
+rpn 8871 quadprime?
+rpn 17 quadprime_
+rpn 18 quintprime
+rpn 18 quintprime_
+rpn random
+rpn 100 randint
+rpn 1 23 range
+rpn 1 23 2 range2
+rpn 6 7 / reciprocal
+rpn 23 5 repunit
+rpn 89 rhombdodec
+rpn 23 riesel
+rpn 8 3 root
+rpn 2 root2
+rpn pi root3
+rpn 4.5 round
+rpn 45 safeprime
+rpn 67 schroeder
+rpn pi 7 / sec
+rpn pi 7 / sech
+rpn 29 sextprime
+rpn 29 sextprime_
+rpn 29 sexyprime
+rpn 29 sexyprime_
+rpn 29 sexyquad
+rpn 29 sexyquad_
+rpn 29 sexytriplet
+rpn 29 sexytriplet_
+rpn -x 0x10 3 shiftleft
+rpn -x 0x1000 4 shiftright
+rpn 32800 short
+rpn pi 2 / sin
+rpn pi 2 / sinh
+rpn 8 9 10 solve2
+rpn 10 -10 10 -10 solve3
+rpn 2 -3 2 -3 2 solve4
+rpn 87 sophieprime
+rpn 8 inches spherearea
+rpn 4 cubic_inches sphereradius
+rpn 5 inches spherevolume
+rpn 45 square
+rpn 34 squaretri
+rpn 3945 steloct
+rpn 19 subfac
+rpn 3948 474 subtract
+rpn 12 superfac
+rpn 89 superprime
+rpn 45 sylvester
+rpn pi 3 / tan
+rpn pi 4 / tanh
+rpn 19978 tetrahedral
+rpn 87 tetranacci
+rpn 3 2 tetrate
+rpn 45 thabit
+rpn 123 456 789 trianglearea
+rpn 203 triangular
+rpn 20706 triangular?
+rpn 10239 tribonacci
+rpn 5588 triplebal
+rpn 5588 triplebal_
+rpn 192834 tripletprime
+rpn 394 truncoct
+rpn 683 trunctet
+rpn 39485 twinprime
+rpn 567 twinprime_
+rpn 290 uchar
+rpn 200 8 uinteger
+rpn 234567890 ulong
+rpn 12345678901234567890 ulonglong
+rpn 7 unitroots
+rpn 23456 ushort
+rpn 0x1939 0x3948 xor
+rpn 14578 seconds ydhms
+rpn 4 zeta
+rpn 142857 ~
+rpn 1 10 range altsign
+rpn 1 10 range altsign2
+rpn 1 10 range altsum
+rpn 1 10 range altsum2
+rpn 1 10 range 45 50 range append
+rpn 1 10 range 2 base
+rpn 1 10 range cf
+rpn gaalon cup convert
+rpn 1 10 range count
+rpn 1 10 range diffs
+rpn 1 100 range gcd
+rpn 1 10 range 1 10 range interleave
+rpn 1 10 range 1 8 range intersection
+rpn 1 10 range 2 5 range 17 linearrecur
+rpn 1 10 range max
+rpn 1 10 range maxindex
+rpn 1 10 range mean
+rpn 1 10 range min
+rpn 1 10 range minindex
+rpn 1 10 range nonzero
+rpn 1 10 range 1 10 range polyadd
+rpn 1 10 range 1 10 range polymul
+rpn [ [ 1 10 range ] [ 1 10 range ] [ 2 11 range ] ] polyprod
+rpn [ [ 1 10 range ] [ 2 11 range ] ] polysum
+rpn 1 10 range 6 polyval
+rpn 1 10 range product
+rpn result
+rpn 1 8 range solve
+rpn 10 1 -1 range2 sort
+rpn 1 10 range sortdesc
+rpn 1 10 range stddev
+rpn 1 10 range sum
+rpn [ 2014 4 30 0 0 0 ] tounixtime
+rpn -c -a30 [ 2 3 2 ] tower
+rpn [ 4 4 4 ] tower2
+rpn 1 10 range 11 20 range union
+rpn 1 10 range unique
+rpn -x 503942034 [ 3 4 5 11 4 4 ] unpack
+rpn 1 10 range zero
+rem rpn _dumpalias
+rem rpn _dumpops
+rem rpn _stats
+@echo off
