1885183b463cb2383ca3e7c24d2eec62185b101b
Rick Gutleber <rickg@his.com>
2014-10-31 10:38:41 -0400

    Oops

--- makeUnits.py
+++ makeUnits.py
@@ -567,11 +567,11 @@ def initializeConversionMatrix( unitConversionMatrix ):
 
     print( 'Saving everything...' )
 
-    if not os.path.isdir( dataPath ):
-        os.makedirs( dataPath )
-
     dataPath = os.path.abspath( os.path.realpath( __file__ ) + os.sep + '..' + os.sep + 'rpndata' )
     fileName = dataPath + os.sep + 'units.pckl.bz2'
+
+    if not os.path.isdir( dataPath ):
+        os.makedirs( dataPath )
 
     with contextlib.closing( bz2.BZ2File( fileName, 'wb' ) ) as pickleFile:
         pickle.dump( PROGRAM_VERSION, pickleFile )
