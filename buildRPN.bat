7635f94b46a409316977fedf056fc6b424a16bc4
Rick Gutleber <rickg@his.com>
2014-11-19 17:20:26 -0500

    build script now works automatically
    Woohoo for automation!
    
    I'll add the version with prime number data next.

--- buildRPN.bat
+++ buildRPN.bat
@@ -1,5 +1,20 @@
+setlocal
 pushd
 call d r
+
+echos VERSION= > version.txt
+type rpnVersion.py | grep PROGRAM_VERSION | cut -d"'" -f2 >> version.txt
+
 python setup_rpn.py build_exe
-rem del /sxyz /nt build
+"C:\Program Files\Inno Setup 5\ISCC.exe" rpn.iss
+
+move Output\setup_rpn.exe "e:\download\setup_rpn-%VERSION%-win32.exe"
+
+del /sxyz /nt build
+del /sxyz /nt Output
+
+del version.txt
+
 popd
+endlocal
+
