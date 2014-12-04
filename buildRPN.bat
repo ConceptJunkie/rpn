32b19e2c030c3e7991b4447f96aeb1c5af00235b
Rick Gutleber <rickg@his.com>
2014-12-04 16:08:56 -0500

    Fixed the build script to use the right name for 32-bit vs 64-bit

--- buildRPN.bat
+++ buildRPN.bat
@@ -1,3 +1,5 @@
+rem TCC script for building the RPN installer
+
 setlocal
 pushd
 call d r
@@ -5,10 +7,16 @@ call d r
 echos VERSION= > version.txt
 type rpnVersion.py | grep PROGRAM_VERSION | cut -d"'" -f2 >> version.txt
 
+set /r version.txt
+
 python setup_rpn.py build_exe
 "C:\Program Files\Inno Setup 5\ISCC.exe" rpn.iss
 
-move Output\setup_rpn.exe "e:\download\setup_rpn-%VERSION%-win32.exe"
+iff %_X64 eq 1 then
+    move Output\setup_rpn.exe "%RPN_TARGET%\setup_rpn-%VERSION%-win64.exe"
+else
+    move Output\setup_rpn.exe "%RPN_TARGET%\setup_rpn-%VERSION%-win32.exe"
+endiff
 
 del /sxyz /nt build
 del /sxyz /nt Output
