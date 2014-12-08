80339d7be43ef4792dd004f6c91158463b7aef57
ConceptJunkie <rickg@his.com>
2014-12-08 10:35:54 -0500

    Working on the build process

--- buildRPN.bat
+++ buildRPN.bat
@@ -10,16 +10,17 @@ type rpnVersion.py | grep PROGRAM_VERSION | cut -d"'" -f2 >> version.txt
 set /r version.txt
 
 python setup_rpn.py build_exe
-"C:\Program Files\Inno Setup 5\ISCC.exe" rpn.iss
 
 iff %_X64 eq 1 then
+    "%PROG32_DIR%\Inno Setup 5\ISCC.exe" rpn64.iss
     move Output\setup_rpn.exe "%RPN_TARGET%\setup_rpn-%VERSION%-win64.exe"
 else
+    "%PROG32_DIR%\Inno Setup 5\ISCC.exe" rpn32.iss
     move Output\setup_rpn.exe "%RPN_TARGET%\setup_rpn-%VERSION%-win32.exe"
 endiff
 
-del /sxyz /nt build
-del /sxyz /nt Output
+rem del /sxyz /nt build
+rem del /sxyz /nt Output
 
 del version.txt
 
