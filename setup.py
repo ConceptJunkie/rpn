6dbe7ea743d7835df12b966f2ecaa597073be19b
Rick Gutleber <rickg@his.com>
2015-07-10 15:23:15 -0400

    Fixed setup.py
    It looks like it works, but I haven't had a chance to test it yet.

--- setup.py
+++ setup.py
@@ -2,7 +2,62 @@ import os
 
 from setuptools import setup, find_packages
 
+def read( *paths ):
+    """Build a file path from *paths* and return the contents."""
+    with open( os.path.join( *paths ), 'r') as f:
+        return f.read( )
+
 setup(
+    name = 'rpn',
+    version = '6.5.0',
+    description = 'command-line RPN calculator with arbitrary precision',
+    long_description = 'TODO: write long description!',
+    #long_description=( read( 'README.rst' ) + '\n\n' +
+    #                   read( 'HISTORY.rst' ) + '\n\n' +
+    #                   read( 'AUTHORS.rst' ) ),
+    url = 'http://github.com/ConceptJunkie/rpn/',
+    license = 'GPL3',
+    author = 'Rick Gutleber',
+    author_email = 'rickg@his.com',
+    py_modules = [ 'rpn',
+                   'rpnCombinatorics',
+                   'rpnComputer',
+                   'rpnConstants',
+                   'rpnDate',
+                   'rpnDeclarations',
+                   'rpnEstimates',
+                   'rpnFactor',
+                   'rpnGeometry',
+                   'rpnGlobals',
+                   'rpnList',
+                   'rpnMath',
+                   'rpnMeasurement',
+                   'rpnModifiers',
+                   'rpnName',
+                   'rpnNumberTheory',
+                   'rpnOperators',
+                   'rpnOutput',
+                   'rpnPolynomials',
+                   'rpnPolytope',
+                   'rpnPrimeUtils',
+                   'rpnPrimes',
+                   'rpnTime',
+                   'rpnUnits',
+                   'rpnUtils',
+                   'rpnVersion' ],
+    install_requires = open( 'requirements.txt' ).read( ).splitlines( ),
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Environment :: Console',
+    ],
     data_files = [ ( 'data_files', [ 'rpndata/balanced_primes.pckl.bz2',
                                      'rpndata/cousin_primes.pckl.bz2',
                                      'rpndata/double_balanced_primes.pckl.bz2',
@@ -25,38 +80,5 @@ setup(
                                      'rpndata/unit_help.pckl.bz2',
                                      'rpndata/units.pckl.bz2' ] ) ],
     packages = find_packages( exclude = [ 'test*', 'setup_*', 'makeRPNPrimes*' ] ),
-)
-
-def read( *paths ):
-    """Build a file path from *paths* and return the contents."""
-    with open( os.path.join( *paths ), 'r') as f:
-        return f.read( )
-
-setup(
-    name = 'rpn',
-    version = '6.5.0',
-    description = 'command-line RPN calculator with arbitrary precision',
-    long_description = 'TODO: write long description!',
-    #long_description=( read( 'README.rst' ) + '\n\n' +
-    #                   read( 'HISTORY.rst' ) + '\n\n' +
-    #                   read( 'AUTHORS.rst' ) ),
-    url = 'http://github.com/ConceptJunkie/rpn/',
-    license = 'GPL3',
-    author = 'Rick Gutleber',
-    author_email = 'rickg@his.com',
-    py_modules = [ 'rpn' ],
-    install_requires = open( 'requirements.txt' ).read( ).splitlines( ),
-    include_package_data = True,
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: End Users/Desktop',
-        'Natural Language :: English',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Environment :: Console',
-    ],
 )
 
