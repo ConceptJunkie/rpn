b0632985fde4ce7aedac2df997a0b0dd99fdbba3
Rick Gutleber <rickg@his.com>
2015-09-15 17:24:11 -0400

    Fixed a few last things in the backup script

--- BackupRPN.bat
+++ BackupRPN.bat
@@ -2,19 +2,18 @@ rem //**************************************************************************
 rem //
 rem //  BackupRPN
 rem //
-rem //  This TCC script is used to backup.   It assumes Cygwin is installed.
+rem //  This TCC script is used to backup RPN.   It assumes Cygwin is installed.
 rem //
 rem //******************************************************************************
 
 setlocal
 
 iff "%1" == "" then
-    echo usage:  BackupRPN target_dir_list versioned_target_dir_list
+    echo usage:  BackupRPN target_dir_list [versioned_target_dir_list]
 	quit
 endiff
 
 set VERSION=%@execstr[grep VERSION rpnVersion.py | cut -d' ' -f3  | tr -d -c 0123456790.]
-set GITHUB_DIR=e:\rick\GitHub\rpn
 set RPN_FILES=rpn*.py;makeHelp.py;makeRPNPrimes.py;makeUnits.py;setup_rpn.py;setup_rpnprimes.py;test*.py;RPNTest.txt;rpn.ico;rpn32.iss;rpn64.iss;BuildRPN.bat;BuildRPNInstaller.bat;tox.ini;pyecm.py;requirements.txt;setup.cfg;MANIFEST.in;setup.py;unpickle.py;preparePrimeData.py;BackupRPN.bat
 
 for /t; %TARGET_DIR in (%1) if exist %TARGET_DIR .and. isdir %TARGET_DIR for %FILE in (%RPN_FILES) copy %FILE %TARGET_DIR
