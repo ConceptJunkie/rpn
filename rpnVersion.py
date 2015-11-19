6838e791b6b535a9e11f8d7a6ef0b83a94069d8c
Rick Gutleber <rickg@his.com>
2015-11-19 18:07:15 -0500

    7.0 alpha 3

--- rpnVersion.py
+++ rpnVersion.py
@@ -18,8 +18,8 @@
 # //
 # //******************************************************************************
 
-PROGRAM_VERSION = '6.99.1'
-PROGRAM_VERSION_NAME = '7.0 alpha 2'
+PROGRAM_VERSION = '6.99.2'
+PROGRAM_VERSION_NAME = '7.0 alpha 3'
 COPYRIGHT_MESSAGE = 'copyright (c) 2015 (1988), Rick Gutleber (rickg@his.com)'
 
 PROGRAM_VERSION_STRING = ' ' + PROGRAM_VERSION + ' (' + PROGRAM_VERSION_NAME + ') - '
