0ec8a47fea0fe7d2d4df584d4877ee32ab375775
Rick Gutleber <rickg@his.com>
2016-06-03 10:25:37 -0400

    that's worth a version number bump

--- rpnVersion.py
+++ rpnVersion.py
@@ -18,8 +18,8 @@
 # //
 # //******************************************************************************
 
-PROGRAM_VERSION = '6.99.20'
-PROGRAM_VERSION_NAME = '7.0a20'
+PROGRAM_VERSION = '6.99.21'
+PROGRAM_VERSION_NAME = '7.0a21'
 COPYRIGHT_MESSAGE = 'copyright (c) 2016 (1988), Rick Gutleber (rickg@his.com)'
 
 PROGRAM_VERSION_STRING = ' ' + PROGRAM_VERSION + ' (' + PROGRAM_VERSION_NAME + ') - '
