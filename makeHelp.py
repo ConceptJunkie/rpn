0021dedb9bce2a6d196c3e8f414fcc7542023240
Rick Gutleber <rickg@his.com>
2018-02-09 11:12:50 -0500

    adding files to let you run rpn the way it's always been run

---
+++ makeHelp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# //******************************************************************************
+# //
+# //  makeHelp
+# //
+# //  RPN command-line calculator help file generator
+# //  copyright (c) 2018, Rick Gutleber (rickg@his.com)
+# //
+# //  License: GNU GPL 3.0 (see <http://www.gnu.org/licenses/gpl.html> for more
+# //  information).
+# //
+# //******************************************************************************
+
+# //******************************************************************************
+# //
+# //  __main__
+# //
+# //******************************************************************************
+
+if __name__ == '__main__':
+    from rpn.makeHelp import main
+    main( )
+
---
+++ makeUnits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# //******************************************************************************
+# //
+# //  makeUnits
+# //
+# //  RPN command-line calculator unit conversion data generator
+# //  copyright (c) 2018, Rick Gutleber (rickg@his.com)
+# //
+# //  License: GNU GPL 3.0 (see <http://www.gnu.org/licenses/gpl.html> for more
+# //  information).
+# //
+# //******************************************************************************
+
+# //******************************************************************************
+# //
+# //  __main__
+# //
+# //******************************************************************************
+
+if __name__ == '__main__':
+    from rpn.makeUnits import main
+    main( )
+
