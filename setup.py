07d7f994fcd42ca4d74a4365fec497558af38b59
Rick Gutleber <rickg@his.com>
2018-02-20 15:09:04 -0500

    A much better long description

--- setup.py
+++ setup.py
@@ -33,15 +33,35 @@ setup(
     version = PROGRAM_VERSION_NAME,
     #description = 'command-line RPN calculator with arbitrary precision',
     description = 'command-line RPN calculator',
-#    long_description =
-#'''
-#rpn is a command-line Reverse-Polish Notation calculator that was first
-#written in C in 1988 as a four-function calculator.  It was rewritten in
-#Python 3 in 2012 and now uses the mpmath library.  It was a Python learning
-#exercise for me, and a fun little toy, but when I found mpmath, it became
-#really cool and powerful, so props to Fredrik Johansson, who did all the
-#heavy lifting (http://mpmath.org).
-#''',
+    long_description =
+'''
+rpnChilada is a command-line Reverse-Polish Notation calculator that was
+first written in C in 1988 as a four-function calculator.
+
+It was rewritten in Python 3 in 2012 and now uses the mpmath library.  It
+was a Python learning exercise for me, and a fun little toy, but when I
+found mpmath, it became really cool and powerful, so props to Fredrik
+Johansson, who did most of the heavy lifting (http://mpmath.org).
+
+rpnChilada gives you the whole enchilada when it comes to playing with
+numbers.  You get math functions, algebra functions, combinatoric functions,
+number theory functions, astronomy functions, extensive unit conversions
+and much, much more.   rpnChilada supports lists, but not matrices.
+
+rpnChilada comes with integrated help, which isn't complete, but has at
+least a basic description of every function, and over 1200 built-in
+examples.
+
+rpnChilada also comes with a fairly extensive test suite, which is being
+constantly improved.
+
+There are still bugs, and since it's a one-person side-project, progress
+is slow, but bug reports and feature requests are welcome at rickg@his.com.
+
+Version 7 has been in the works for more than two and a half years, and the
+number of operators since version 6.4 has increased by at least 200.  It also
+supports user-defined variables and functions.
+''',
 
     long_description =
 '''
