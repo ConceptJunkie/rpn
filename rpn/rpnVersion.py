bf8739c6e85e767040d149263588b8e7c0fd81a4
Rick Gutleber <rickg@his.com>
2019-04-02 15:08:20 -0400

    updated to version 8.0.beta2

--- rpn/rpnVersion.py
+++ rpn/rpnVersion.py
@@ -19,8 +19,8 @@
 # //******************************************************************************
 
 PROGRAM_NAME = 'rpnChilada'
-PROGRAM_VERSION = '7.999.1'
-PROGRAM_VERSION_NAME = '8.0.beta1'
+PROGRAM_VERSION = '7.999.2'
+PROGRAM_VERSION_NAME = '8.0.beta2'
 COPYRIGHT_MESSAGE = 'copyright (c) 2019 (1988), Rick Gutleber (rickg@his.com)'
 
 PROGRAM_VERSION_STRING = ' ' + PROGRAM_VERSION + ' (' + PROGRAM_VERSION_NAME + ')'
