235ab301cbf241506af2d43b7089d30174c3fcf2
Rick Gutleber <rickg@his.com>
2019-04-02 14:29:34 -0400

    added release notes

--- rpn/makeHelp.py
+++ rpn/makeHelp.py
@@ -1054,6 +1054,26 @@ release notes, so there are a ton of new features not mentioned here.
     '''
 For notes about earlier versions, use 'help old_release_notes'.
 
+8.0
+
+The unit conversion code has been heavily refactored and works much better now.
+
+Added the 'base_units' and 'dimensions' operators, mostly for testing purposes.
+
+Added _dump_conversions, also for testing purposes.
+
+rpnChilada is now smart enough to recognize when an OEIS request has failed,
+and to ignore the cached result stored as a result.  If it detects that the
+cached value is empty, it will perform the request again and recache the
+result.
+
+Help now supports units and constant operators after way too long.  Filling in
+the help info for the units and constant operators, along with all the existing
+help info that's missing, will take a while, and is continuing.
+
+The astronomy functionality has been refactored to support migrating to the
+skyfield library from pyephem.
+
 7.2.5
 
 I fat-fingered an addition to the requirements.txt file.  :-/
