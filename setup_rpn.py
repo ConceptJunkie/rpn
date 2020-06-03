737ad36b7a7a1be66323eeb7261438c2746c8dc4
Rick Gutleber <rickg@his.com>
2020-06-03 10:01:04 -0400

    cx_freeze finally works again
    So I'm working on getting a Windows executable.

--- setup_rpn.py
+++ setup_rpn.py
@@ -1,6 +1,5 @@
 from cx_Freeze import setup, Executable
 from rpn.rpnVersion import PROGRAM_VERSION
-
 import rpn.rpnGlobals as g
 
 import os
@@ -13,13 +12,32 @@ buildOptions = \
         ],
 
         include_files = [
-            "rpn" + os.sep + g.dataDir + os.sep + 'units.pckl.bz2',
-            "rpn" + os.sep + g.dataDir + os.sep + 'unit_conversions.pckl.bz2',
-            "rpn" + os.sep + g.dataDir + os.sep + 'unit_help.pckl.bz2',
-            "rpn" + os.sep + g.dataDir + os.sep + 'unit_names.pckl.bz2',
-            "rpn" + os.sep + g.dataDir + os.sep + 'deltat.data',
-            "rpn" + os.sep + g.dataDir + os.sep + 'deltat.preds',
-            "rpn" + os.sep + g.dataDir + os.sep + 'Leap_Second.dat',
+            'rpn' + os.sep + g.dataDir + os.sep + 'units.pckl.bz2',
+            'rpn' + os.sep + g.dataDir + os.sep + 'unit_conversions.pckl.bz2',
+            'rpn' + os.sep + g.dataDir + os.sep + 'unit_help.pckl.bz2',
+            'rpn' + os.sep + g.dataDir + os.sep + 'unit_names.pckl.bz2',
+            'rpn' + os.sep + g.dataDir + os.sep + 'deltat.data',
+            'rpn' + os.sep + g.dataDir + os.sep + 'deltat.preds',
+            'rpn' + os.sep + g.dataDir + os.sep + 'Leap_Second.dat',
+            'rpn' + os.sep + g.dataDir + os.sep + 'balanced_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'cousin_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'double_balanced_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'huge_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'isolated_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'large_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'quadruple_balanced_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'quad_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'quint_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'sext_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'sexy_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'sexy_quadruplets.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'sexy_triplets.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'small_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'sophie_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'super_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'triplet_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'triple_balanced_primes.cache',
+            'rpn' + os.sep + g.dataDir + os.sep + 'twin_primes.cache',
         ],
 
         include_msvcr = 1,
@@ -33,7 +51,43 @@ executables = [
                 initScript = None,
                 shortcutName = 'rpnChilada ' + PROGRAM_VERSION,
                 targetName = 'rpn.exe'
+    ),
+
+    Executable( script = 'preparePrimeData.py',
+                base = None,
+                icon = 'rpn.ico',
+                initScript = None,
+                shortcutName = 'rpnChilada Prepare Prime Data ' + PROGRAM_VERSION,
+                targetName = 'rpnPreparePrimeData.exe'
+    ),
+
+    Executable( script = 'makeHelp.py',
+                base = None,
+                icon = 'rpn.ico',
+                initScript = None,
+                shortcutName = 'rpnChilada Make Help ' + PROGRAM_VERSION,
+                targetName = 'rpnMakeHelp.exe'
+    ),
+
+    Executable( script = 'makeUnits.py',
+                base = None,
+                icon = 'rpn.ico',
+                initScript = None,
+                shortcutName = 'rpnChilada Make Units ' + PROGRAM_VERSION,
+                targetName = 'rpnMakeUnits.exe'
+    ),
+
+    Executable( script = 'testRPN.py',
+                base = None,
+                icon = 'rpn.ico',
+                initScript = None,
+                shortcutName = 'rpnChilada Test ' + PROGRAM_VERSION,
+                targetName = 'testRPN.exe'
     )
+
+
+
+
 ]
 
 setup( name = 'rpn',
