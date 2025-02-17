30818be95190044cae6f6d87999ea3ca38e72210
Rick Gutleber <rickg@his.com>
2025-02-17 14:10:09 -0500

    updating copyright to 2025

--- rpn/test/testConvert.py
+++ rpn/test/testConvert.py
@@ -5,7 +5,7 @@
 #  testConvert
 #
 #  test script for RPN unit conversion
-#  copyright (c) 2024, Rick Gutleber (rickg@his.com)
+#  copyright (c) 2025, Rick Gutleber (rickg@his.com)
 #
 #  License: GNU GPL 3.0 (see <http://www.gnu.org/licenses/gpl.html> for more
 #  information).
--- rpn/units/rpnConstantOperators.py
+++ rpn/units/rpnConstantOperators.py
@@ -5,7 +5,7 @@
 #  rpnConstantOperators.py
 #
 #  rpnChilada constant operator declarations
-#  copyright (c) 2024, Rick Gutleber (rickg@his.com)
+#  copyright (c) 2025, Rick Gutleber (rickg@his.com)
 #
 #  License: GNU GPL 3.0 (see <http://www.gnu.org/licenses/gpl.html> for more
 #  information).
